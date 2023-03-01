51106ae6106941559ef6c2ab9da425ca0affcbc9
Rimvydas <tryheadas@gmail.com>
2023-03-01 16:20:23 +0200

    Complete v0.3

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
-main: main.cpp
-	g++ -O3 -std=c++2a main.cpp -o main.app && ./main.app
+main: main.cpp funtions.o
+	g++ -O3 -std=c++2a main.cpp funtions.o -o main.app
+
+funtions.o: funtions.cpp funtions.h
+	g++ -O3 -std=c++2a -c funtions.cpp
+
+run: main
+	./main.app
 
 clean:
-	rm -rf *.app
+	rm -rf *.app *.o
--- MyLib.h
+++ MyLib.h
@@ -30,19 +30,3 @@ using std::stringstream;
 using std::stof;
 using std::exception;
 using std::invalid_argument;
-
-struct Student {
-    string name, surname;
-    vector<float> homeworkGrades;
-    int examGrade;
-    double finalGradeAverage = 0, finalGradeMedian = 0;
-};
-
-void addStudents(vector<Student> &students);
-void addStudentsFromFile(vector<Student> &students);
-void addStudentsFromSTDIN(vector<Student> &students);
-void enterStudentData(Student &student);
-void generateGrades(Student &student);
-void calculateFinalGrade(Student &student);
-string getFormatedStudentData(Student student, bool useAverage);
-void writeResultsToFile(vector<Student> &students, string fileName);
